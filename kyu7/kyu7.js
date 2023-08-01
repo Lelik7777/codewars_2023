83ffc73a555c45da62caf75a30a23376e1183160
Lelik Jan <21alex7777@gmail.com>
2023-08-01 12:54:21 +0300

    feat: Training JS #27: methods of arrayObject---filter()

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -268,3 +268,49 @@ const isolateIt1 = (arr) =>
 
 console.log(isolateIt1(["abcd", "efgh", "adscd"]));
 
+//? //////////////////////////////////////////////////////////////////////////
+
+//? 10. Training JS #27: methods of arrayObject---filter()
+
+// Coding in function countGrade. function accept 1 parameters scores, it's a number array. Your task is to count the grade distribution of the scores, to return an object like this:
+
+// {S:888, A:888, B:888, C:888, D:888, X:888}
+// Grading rules:
+
+// Grade S: Full marks(score=100)
+// Grade A: score<100 and score>=90
+// Grade B: score<90 and score>=80
+// Grade C: score<80 and score>=60
+// Grade D: score<60 and score>=0
+// Grade X: score=-1(The cheating guy gets a score like that)
+// Example
+// countGrade([50,60,70,80,90,100]) should return {S:1, A:1, B:1, C:2, D:1, X:0}
+// countGrade([65,75,,85,85,95,100,100]) should return {S:2, A:1, B:2, C:2, D:0, X:0}
+// countGrade([-1,-1,-1,-1,-1,-1]) should return {S:0, A:0, B:0, C:0,D:0, X:6}
+
+//приходит массив баллов, а на выходе объект,в котором каждая оценка превращена в значение объекта; таким образом происходит заполнение массива
+
+const countGrade = (arr) => {
+  const obj = { S: 0, A: 0, B: 0, C: 0, D: 0, X: 0 };
+  obj.S = arr.filter((el) => el === 100).length;
+  obj.A = arr.filter((el) => el < 100 && el >= 90).length;
+  obj.B = arr.filter((el) => el < 90 && el >= 80).length;
+  obj.C = arr.filter((el) => el < 80 && el >= 60).length;
+  obj.D = arr.filter((el) => el < 60 && el >= 0).length;
+  obj.X = arr.filter((el) => el === -1).length;
+  return obj;
+};
+console.log(countGrade([50, 60, 70, 80, 90, 100]));
+
+const countGrade1 = (arr) => {
+  const count = (fun) => arr.filter((el) => fun(el)).length;
+  return {
+    S: count((x) => x === 100),
+    A: count((x) => x < 100 && x >= 90),
+    B: count((x) => x < 90 && x >= 80),
+    C: count((x) => x < 80 && x >= 60),
+    D: count((x) => x < 60 && x >= 0),
+    X: count((x) => x === -1),
+  };
+};
+console.log(countGrade1([50, 60, 70, 80, 90, 100]));
