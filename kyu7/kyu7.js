90c96efc86bc825a3f26346f5f42f4b9cd69d9cc
Lelik Jan <21alex7777@gmail.com>
2023-08-03 12:45:54 +0300

    feat:add solution for Training JS #30: methods of arrayObject---reduce() and reduceRight()

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -416,6 +416,60 @@ bigToSmall([
   [5, 6],
 ]);
 
+//? //////////////////////////////////////////////////////////////////////////
 
+console.log([1, 3, 4, 5, 6, 7].reduce((acc, cur) => acc + (cur % 2 ? 1 : 0)));
+
+console.log([1, 3, 4].reduce((acc, cur) => acc.concat(cur + 2), []));
+console.log([1, 3, 4].reduce((acc, cur) => [...acc, cur + 2], []));
+
+var arr1 = [1, 2, 3, 4, 5];
+var arr2 = [6, 7, 8, 9, 10];
+//var result=arr1.map(x=>x*x).reduce((a,b,i,ar)=>a.concat(ar[i],arr2[i]),[])
+//ES6 syntax
+var result = arr1
+  .map((x) => x * x)
+  .reduce((a, b, i, ar) => [...a, ar[i], arr2[i]], []);
+
+console.log(result); //output: [ 1, 6, 4, 7, 9, 8, 16, 9, 25, 10 ]
 
 //? //////////////////////////////////////////////////////////////////////////
+
+//? 13. Training JS #30: methods of arrayObject---reduce() and reduceRight()
+
+// Coding in function tailAndHead. function accept 1 parameter arr(a number array).
+
+// We use an example to explain this task:
+
+// You got an array [123,456,789,12,34,56,78].
+
+// First, from left to right, the tail of an element and the head of the next element are added together. The results are put into an array. like this:
+
+// [123,456,789,1 2,3 4,5 6,78]
+//    3+4 6+7 9+1 2+3 4+5 6+7
+//     |   |   |   |   |   |
+//    [7 ,13 ,10 , 5 , 9 ,13]
+// And then, Calculate their product and return it(7x13x10x5x9x13=532350)
+
+// So, tailAndHead([123,456,789,12,34,56,78]) should return 532350
+
+// All elements of arr are positive integer. If a number is less than 10, its head and tail are the same. Please try to use reduce() solve this kata.
+
+// Examples
+// tailAndHead([1,2,3,4,5]) should return 945
+// tailAndHead([111,2345,66,78,900]) should return 7293
+// tailAndHead([35456,782,569,2454,875]) should return 12012
+
+// 1. каждое число массива превратить в строку и получить первый элемент и последний элемент этого числа,использую slice() 2. используя reduce, суммировать эти элементы 3. получившиеся суммы перемножить также через reduce()
+
+const tailAndHead = (arr) =>
+  arr
+    .map((num, i, ar) =>
+      i < arr.length - 1
+        ? Number(String(num).slice(-1)) + Number(String(ar[i + 1]).slice(0, 1))
+        : ""
+    )
+    .filter((x) => x !== "")
+    .reduce((acc, cur) => acc * cur);
+
+console.log(tailAndHead([123, 456, 789, 12, 34, 56, 78]));
