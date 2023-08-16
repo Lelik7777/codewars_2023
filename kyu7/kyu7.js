d772e4032629f4c6080e9e1aaef7d79dee41a048
Lelik Jan <21alex7777@gmail.com>
2023-08-16 15:47:35 +0300

    feat: add solution for Training JS #41: Regular Expression--"\"

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -888,3 +888,59 @@ const regex1=/(http|https):\/\/[a-z0-9\.]+(\.net|\.com)/gi;
 const regex2=/https?:\/\/[a-z0-9\.]+\.(net|com)/gi;
 console.log('http://hEllo233.44.com'.match(regex1));
 console.log("http://www.codewars1.com!@#$%http://www.codewars2.net".match(regex2));
+
+//? //////////////////////////////////////////////////////////////////////////
+
+//? 23.Training JS #41: Regular Expression--"\"
+
+function isPair(s){
+  var r=/\(\)|\[\]|\{\}/g;
+  while (r.test(s)) s=s.replace(r,"");
+  return s=="";
+}
+
+console.log( isPair("()") )     //output: true
+console.log( isPair("((()))") ) //output: true
+
+console.log( isPair(")(") )     //output: false
+console.log( isPair("({[})") )  //output: false
+
+//You might have guessed the task. Yes, This time your task is to write a regular expressions matching all palindrome substring of a string.
+
+// The rules:
+
+// The string contains letters, numbers, underscores and spaces. space is the separator. the substring means the whole of a substring that separated by spaces. For example:
+// "aaa bcccd" should match a substring "aaa" ,
+// should not match "ccc", "ccc" is a part of substring "bcccd"
+// Palindrome substring length range is 2-7. It means don't match the substring which length=1 or length>7.
+// A string may contain multiple palindrome substring.
+// You should not ignore case.
+// Your regular expression name should be regex and your result should be a string array.
+// Examples
+// "aa bbb cccc".match(regex)             should return [ 'aa', 'bbb', 'cccc' ]
+// "aaa bcccd".match(regex)               should return [ 'aaa' ]
+// "_x_x_ --- ~~|~~".match(regex)         should return [ '_x_x_' ]
+// "ABCDCBA ABABABA".match(regex)         should return [ 'ABCDCBA', 'ABABABA' ]
+// "121 1221 13577531 11211".match(regex) should return [ '121', '1221', '11211' ]
+// "aabbbcccc d".match(regex)             should return null
+// "1    1".match(regex)                  should return null
+// "abbA CdDc".match(regex)               should return null
+
+//скопировал, так и не смог разобраться
+var regex02=/\b(\w)(\w)?(\w)?\w?\3\2\1\b/g
+console.log("qbcddcbq".match(regex02));
+
+//    \b - spaces around the words / numbers
+//    (\w) - first symbol
+//    (\w)? - find out how many symbols in word/number
+//    \b(\w)(\w)?(\w)?\w?\3\2\1\b checking for palyndrome
+//        1   2    3    4 5 6 7
+
+//    7th symbol must match 1 sub expression, 6 = 2, 5 = 3 and 4th symbol in the middle.
+//    So \3 checks if the 5th symbol equal 3rd subexpression, /2 - if the 6th symbol match 2nd subexpression, /1 - if the 7th symbol math 1st subexpression.
+
+//    for example str   '   A   B   C   D  C B A  '
+//    regex             \b(\w)(\w)(\w)(\w)\3\2\1\\b
+//    sub expression №      1   2   3   4
+
+//    Did I understand this solution correctly?
