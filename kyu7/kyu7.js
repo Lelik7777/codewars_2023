2f7a8f0c448591f7f85c9c47bd033cc5c3b84b48
Lelik Jan <21alex7777@gmail.com>
2023-08-21 15:12:00 +0300

    feat: add solution for 80 Kids Punky Brewste  Socks

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1045,3 +1045,35 @@ console.log(
 ..........
 ..........`)
 );
+
+//? //////////////////////////////////////////////////////////////////////////
+
+//? 27.80's Kids #3: Punky Brewster's Socks
+
+// Punky loves wearing different colored socks, but Henry can't stand it.
+
+// Given an array of different colored socks, return a pair depending on who was picking them out.
+
+// Example:
+
+// getSocks('Punky', ['red','blue','blue','green']) -> ['red', 'blue']
+// Note that Punky can have any two colored socks, in any order, as long as they are different and both exist. Henry always picks a matching pair.
+
+// If there is no possible combination of socks, return an empty array.
+
+const getSocks = (name, arr) => {
+  if (name === "Henry") {
+    const set = new Set(arr);
+    const res = arr
+      .map((el) => (set.has(el) ? set.delete(el) : el))
+      .filter((x) => x !== true);
+    return [...res, ...res];
+  }
+
+  if (name === "Punky") {
+    const res = [...new Set(arr)];
+    return res.length > 1 ? [res[0], res[1]] : [];
+  }
+};
+
+console.log(getSocks("Henry", ["", "hot", "set", "blue"]));
