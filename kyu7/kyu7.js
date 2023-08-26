ea7ee80e4b997c914b87584dd03419ae7675599a
Lelik Jan <21alex7777@gmail.com>
2023-08-26 15:51:51 +0300

    feat: add solution for Unflatten a list (Easy)

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1387,7 +1387,86 @@ const scoreboard = (str) => {
 console.log(scoreboard("The score is four nil"));
 
 function scoreboard1(string) {
-  var dict = {'nil': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
-              'six': 6, 'seven': 7, 'eight': 8, 'nine': 9};
-  return string.match(/(nil|one|two|three|four|five|six|seven|eight|nine)/g).map(e => dict[e]);
+  var dict = { nil: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
+  return string.match(/(nil|one|two|three|four|five|six|seven|eight|nine)/g).map((e) => dict[e]);
 }
+
+//? //////////////////////////////////////////////////////////////////////////
+
+//? 37.Convert the score
+console.log(
+  "34 45 54"
+    .split(" ")
+    .map((x, i) => (i < 2 ? x : ""))
+    .filter((x) => x !== "")
+);
+
+const obj = {
+  name: "bob",
+  age: 323,
+  job: "admin",
+  who: "man",
+};
+
+function getInfo({ name, job }) {
+  console.log("name is ", name, "job is ", job);
+}
+getInfo(obj);
+
+//? //////////////////////////////////////////////////////////////////////////
+
+//? 38.Unflatten a list (Easy)
+
+// There are several katas like "Flatten a list". These katas are done by so many warriors, that the count of available list to flattin goes down!
+
+// So you have to build a method, that creates new arrays, that can be flattened!
+
+// #Shorter: You have to unflatten a list/an array.
+
+// You get an array of integers and have to unflatten it by these rules:
+
+// - You start at the first number.
+// - If this number x is smaller than 3, take this number x direct
+//   for the new array and continue with the next number.
+// - If this number x is greater than 2, take the next x numbers (inclusive this number) as a
+//   sub-array in the new array. Continue with the next number AFTER this taken numbers.
+// - If there are too few numbers to take by number, take the last available numbers.
+// The given array will always contain numbers. There will only be numbers > 0.
+
+// Example:
+
+//  [1,4,5,2,1,2,4,5,2,6,2,3,3] -> [1,[4,5,2,1],2,[4,5,2,6],2,[3,3]]
+
+// Steps:
+// 1. The 1 is added directly to the new array.
+// 2. The next number is 4. So the next 4 numbers (4,5,2,1) are added as sub-array in the new array.
+// 3. The 2 is added directly to the new array.
+// 4. The next number is 4. So the next 4 numbers (4,5,2,6) are added as sub-array in the new array.
+// 5. The 2 is added directly to the new array.
+// 6. The next number is 3. So the next 3 numbers would be taken. There are only 2,
+//    so take these (3,3) as sub-array in the new array.
+
+//1. проверить текущеее число меньше ли оно 3, 2.если да,то в новый массив, 3.если нет,то количество элементов данного числа в подмассив и в новый массив
+
+const unflatten = (arr) => {
+  const res = [];
+  while (arr.length > 0) {
+    for (let i = 0; i < 1; i++) {
+      if (arr[i] < 3) {
+        res.push(arr.shift());
+      } else {
+        res.push(arr.splice(i, arr[i]));
+      }
+    }
+  }
+  return res;
+};
+
+const unflatten1 = (arr) => {
+  const res = [];
+  while (arr.length) {
+    res.push(arr[0] < 3 ? arr.shift() : arr.splice(0, arr[0]));
+  }
+  return res;
+};
+console.log(unflatten1([3, 5, 2, 1]));
