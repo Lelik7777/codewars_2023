f793126faca19145eab29c6e339cf808ec561155
Lelik Jan <21alex7777@gmail.com>
2023-08-29 12:49:32 +0300

    feat: add solution for Thinkful - List and Loop Drills: Lists of lists

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1470,3 +1470,39 @@ const unflatten1 = (arr) => {
   return res;
 };
 console.log(unflatten1([3, 5, 2, 1]));
+
+//? //////////////////////////////////////////////////////////////////////////
+
+//? 39.Thinkful - List and Loop Drills: Lists of lists
+
+// You have a two-dimensional list in the following format:
+
+// data = [[2, 5], [3, 4], [8, 7]]
+// Each sub-list contains two items, and each item in the sub-lists is an integer.
+
+// Write a function process_data()/processData() that processes each sub-list like so:
+
+// [2, 5] --> 2 - 5 --> -3
+// [3, 4] --> 3 - 4 --> -1
+// [8, 7] --> 8 - 7 --> 1
+// and then returns the product of all the processed sub-lists: -3 * -1 * 1 --> 3.
+
+// For input, you can trust that neither the main list nor the sublists will be empty.
+
+const processData = (list) => list.map((sublis) => sublis[0] - sublis[1]).reduce((a, c) => a * c, 1);
+
+console.log(
+  processData([
+    [2, 5],
+    [3, 4],
+    [8, 7],
+  ])
+);
+const process_data = (arr) => arr.reduce((a, [b, c]) => a * (b - c), 1);
+console.log(
+  process_data([
+    [2, 5],
+    [3, 4],
+    [8, 7],
+  ])
+);
