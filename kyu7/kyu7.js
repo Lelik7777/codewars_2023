9ac26b949db1ecf5dde6a10d6a00d53d7a777963
Lelik Jan <21alex7777@gmail.com>
2023-09-04 15:49:15 +0300

    feat: add solution for Strings, strings, strings (Easy)

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1669,3 +1669,40 @@ console.log(
 );
 const arrNum = [3, 4, 5];
 console.log(arrNum.slice(0, -1));
+
+//? //////////////////////////////////////////////////////////////////////////
+
+// //? 45.Strings, strings, strings (Easy)
+
+// The toString() method has been disabled for booleans, numbers, arrays and objects. Your goal is to retrive toString() for the following data types.
+
+// I. Booleans
+// For booleans:
+
+// true should be converted to "true"
+// false should be converted to "false"
+// II. Numbers
+// For numbers, conversion should be as follows:
+
+// e.g.
+// (3).toString() === "3"
+// (3.14).toString() === "3.14"
+// (-78).toString() === "-78"
+// Math.PI.toString() === "3.141592653589793"
+// III. Arrays
+// For the purposes of this Kata, you will only be expected to convert arrays with numbers only into strings. However, on top of fixing it, we would also like to improve it as well. We would like to keep the square brackets ([]) around the "stringified" array as it would be seen in Javascript code. For example:
+
+ // e.g.
+// [].toString() === "[]"
+// [1].toString() === "[1]"
+// [2,4,8,17].toString() === "[2, 4, 8, 17]"
+
+Boolean.prototype.toString=Number.prototype.toString=Array.prototype.toString=function(){
+  return JSON.stringify(this)
+}
+console.log(true.toString());
+console.log([2,4,5].toString());
+
+//? //////////////////////////////////////////////////////////////////////////
+
+// //? 46.Thinkful - String Drills: Areacode extractor
