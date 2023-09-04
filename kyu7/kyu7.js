95ecb06942e3f25df95b2ddf6552d42267501327
Lelik Jan <21alex7777@gmail.com>
2023-09-04 16:08:10 +0300

    feat: add solution for String Drills: Areacode extractor

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1706,3 +1706,18 @@ console.log([2,4,5].toString());
 //? //////////////////////////////////////////////////////////////////////////
 
 // //? 46.Thinkful - String Drills: Areacode extractor
+// You've got a bunch of textual data with embedded phone numbers. Write a function area_code() that finds and returns just the area code portion of the phone number.
+
+// >>> message = "The supplier's phone number is (555) 867-5309"
+// >>> area_code(message)
+// '555'
+// The returned area code should be a string, not a number. Every phone number is formatted like in the example, and the only non-alphanumeric characters in the string are apostrophes ' or the punctuation used in the phone number.
+
+function areaCode(text) {
+return text.match(/\(\d+\)/)[0].replace(/[()]/g, '');
+}
+console.log(areaCode2("The 102nd district court's fax line is (124) 816-3264"));
+
+function areaCode2(text){
+  return text.slice(text.indexOf('(')+1,text.indexOf(')'))
+}
