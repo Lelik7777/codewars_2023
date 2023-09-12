ae61e39b1bca58e43661cc2c0c90cf1df9b63a58
Lelik Jan <21alex7777@gmail.com>
2023-09-12 11:23:53 +0300

    feat: add solution for task:.Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1867,3 +1867,24 @@ console.log(getFirstPython([
   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
 ]));
+
+//? //////////////////////////////////////////////////////////////////////////
+
+// //? 51.Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+// For example, given the following list:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+// your function should return number 1.
+
+// If, there are no JavaScript developers from Europe then your function should return 0.
+
+const countDevelopers=(list) => list.filter(dev=>dev.language==='JavaScript'&&dev.continent==='Europe').length;
