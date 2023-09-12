e68b017d1bee76f32f0f6035350cf9652e0d9b36
Lelik Jan <21alex7777@gmail.com>
2023-09-12 10:47:28 +0300

    feat: add solution for task:Higher-Order Functions Series - Find the first Python developer

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1817,7 +1817,53 @@ console.log(sortByValueAndIndex([-3, -12, -24, -6, 29, 16, -26, -29, 21, -1, 26,
 //     ---> should return 1
 
 function solution(fullText, searchText) {
-  return (fullText.match(new RegExp(searchText, "g"))||[]).length
+  //проверяем,если null, return []
+  return (fullText.match(new RegExp(searchText, "g")) || []).length;
 }
 console.log(solution("ccddeeccddeecc", "gg"));
 console.log(solution("abbc", "bb"));
+
+//? //////////////////////////////////////////////////////////////////////////
+
+// //? 50. Higher-Order Functions Series - Find the first Python developer
+
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+// Your task is to return one of the following strings:
+
+// < firstName here >, < country here > of the first Python developer who has signed up; or
+// There will be no Python developers if no Python developer has signed up.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ];
+// your function should return Victoria, Puerto Rico.
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+
+// This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
+
+const getFirstPython = (list) => {
+  for(let i=0;i<list.length;i++){
+    if(list[i].language==='Python'){
+      return [list[i].firstName, list[i].country].join(", ");
+    }
+  }
+  return "There will be no Python developers";
+
+};
+var list2 = [
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+];
+
+console.log(getFirstPython([
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+]));
