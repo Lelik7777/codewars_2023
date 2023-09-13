d853c12be7f45067816f776d7c9081388f140bd0
Lelik Jan <21alex7777@gmail.com>
2023-09-13 12:09:40 +0300

    feat: solve task:  Higher-Order Functions Series - Greet developers

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -1849,24 +1849,39 @@ console.log(solution("abbc", "bb"));
 // This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
 
 const getFirstPython = (list) => {
-  for(let i=0;i<list.length;i++){
-    if(list[i].language==='Python'){
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].language === "Python") {
       return [list[i].firstName, list[i].country].join(", ");
     }
   }
   return "There will be no Python developers";
-
 };
 var list2 = [
-  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
-  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+  { firstName: "Kseniya", lastName: "T.", country: "Belarus", continent: "Europe", age: 29, language: "JavaScript" },
+  {
+    firstName: "Amar",
+    lastName: "V.",
+    country: "Bosnia and Herzegovina",
+    continent: "Europe",
+    age: 32,
+    language: "Ruby",
+  },
 ];
 
-console.log(getFirstPython([
-  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
-  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
-  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
-]));
+console.log(
+  getFirstPython([
+    { firstName: "Mark", lastName: "G.", country: "Scotland", continent: "Europe", age: 22, language: "JavaScript" },
+    {
+      firstName: "Victoria",
+      lastName: "T.",
+      country: "Puerto Rico",
+      continent: "Americas",
+      age: 30,
+      language: "Python",
+    },
+    { firstName: "Emma", lastName: "B.", country: "Norway", continent: "Europe", age: 19, language: "Clojure" },
+  ])
+);
 
 //? //////////////////////////////////////////////////////////////////////////
 
@@ -1887,4 +1902,52 @@ console.log(getFirstPython([
 
 // If, there are no JavaScript developers from Europe then your function should return 0.
 
-const countDevelopers=(list) => list.filter(dev=>dev.language==='JavaScript'&&dev.continent==='Europe').length;
+const countDevelopers = (list) =>
+  list.filter((dev) => dev.language === "JavaScript" && dev.continent === "Europe").length;
+
+//? //////////////////////////////////////////////////////////////////////////
+
+// //? 52. Higher-Order Functions Series - Greet developers
+
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+// Hi < firstName here >, what do you like the most about < language here >?
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+// ];
+// your function should return the following array:
+
+// [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+//     greeting: 'Hi Sofia, what do you like the most about Java?'
+//   },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+//     greeting: 'Hi Lukas, what do you like the most about Python?'
+//   },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+//     greeting: 'Hi Madison, what do you like the most about Ruby?'
+//   }
+// ];
+
+const greetingDevelopers = (list) => list.map((developer) => ({ ...developer, greeting: `Hi ${developer.firstName}, what do you like the most about ${developer.language}?` }));
+console.log(
+  greetingDevelopers([
+    { firstName: "Sofia", lastName: "I.", country: "Argentina", continent: "Americas", age: 35, language: "Java" },
+    { firstName: "Lukas", lastName: "X.", country: "Croatia", continent: "Europe", age: 35, language: "Python" },
+    {
+      firstName: "Madison",
+      lastName: "U.",
+      country: "United States",
+      continent: "Americas",
+      age: 32,
+      language: "Ruby",
+    },
+  ])
+);
