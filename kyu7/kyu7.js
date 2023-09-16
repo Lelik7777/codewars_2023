2d523cfd3b6a69489ce2a0dc041ea5be9ba75578
Lelik Jan <21alex7777@gmail.com>
2023-09-16 12:19:24 +0300

    feat: add solution for task: Higher-Order Functions Series - Find the most senior developer

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -2024,10 +2024,17 @@ const countLanguages2 = (list) =>
   }, {});
 console.log(
   countLanguages2([
-    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
-    { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
-    { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
-    { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+    { firstName: "Noah", lastName: "M.", country: "Switzerland", continent: "Europe", age: 19, language: "C" },
+    {
+      firstName: "Anna",
+      lastName: "R.",
+      country: "Liechtenstein",
+      continent: "Europe",
+      age: 52,
+      language: "JavaScript",
+    },
+    { firstName: "Ramon", lastName: "R.", country: "Paraguay", continent: "Americas", age: 29, language: "Ruby" },
+    { firstName: "George", lastName: "B.", country: "England", continent: "Europe", age: 81, language: "C" },
   ])
 );
 //? //////////////////////////////////////////////////////////////////////////
@@ -2048,4 +2055,7 @@ console.log(
 // ];
 // your function should return true.
 
-const isSameLanguage=(list)=>list.every((developer)=>developer.language===list[0].language)
+const isSameLanguage = (list) => list.every((developer) => developer.language === list[0].language);
+//? //////////////////////////////////////////////////////////////////////////
+
+// //? 56.
