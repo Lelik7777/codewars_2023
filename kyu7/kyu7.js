dc9cb1a4ff29d4c7ea6832f120b7ddff35e48a91
Lelik Jan <21alex7777@gmail.com>
2023-09-16 11:43:45 +0300

    feat:add solution for task: Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?

--- kyu7/kyu7.js
+++ kyu7/kyu7.js
@@ -2019,7 +2019,7 @@ countLanguages([
 
 const countLanguages2 = (list) =>
   list.reduce((a, c, i) => {
-    a[c.language] = (a[c.language] || 0) + 1;
+    a[c.language] = (a[c.language] ?? 0) + 1;
     return a;
   }, {});
 console.log(
@@ -2030,3 +2030,22 @@ console.log(
     { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
   ])
 );
+//? //////////////////////////////////////////////////////////////////////////
+
+// //? 55.Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language?
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return either:
+
+// true if all developers in the list code in the same language; or
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+// ];
+// your function should return true.
+
+const isSameLanguage=(list)=>list.every((developer)=>developer.language===list[0].language)
