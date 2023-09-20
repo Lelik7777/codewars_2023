b33b6359a66d40875effd38b7d0499bb748fdac8
Lelik Jan <21alex7777@gmail.com>
2023-09-20 18:14:28 +0300

    feat:add solution for task: CCoding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?

--- kyu6/kyu6.js
+++ kyu6/kyu6.js
@@ -156,7 +156,7 @@ console.log(
 );
 //? //////////////////////////////////////////////////////////////////////////
 
-  //? 7.Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?
+//? 7.Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?
 // You will be given a sequence of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
 
 // Your task is to return:
@@ -179,4 +179,35 @@ console.log(
 // The input array and continent names will always be valid and formatted as in the list above for example 'Africa' will always start with upper-case 'A'.
 //1. get array continents 2. get Set form this array 3. compare it`s length - must be === 5
 
-const allContinents=list=>[...new Set(list.map(dev=>dev.continent))].length>=5;
+const allContinents = (list) => [...new Set(list.map((dev) => dev.continent))].length >= 5;
+console.log("hello");
+
+//? //////////////////////////////////////////////////////////////////////////
+
+//? 8.CCoding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse?
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties, fifties, sixties, seventies, eighties, nineties, centenarian (at least 100 years young).
+// false otherwise.
+
+const isAgeDiverse = (list) => {
+  console.log(list);
+  [...new Set(list.map((dev) => Math.floor(dev.age / 10)))].length >= 10;
+};
+
+console.log(
+  isAgeDiverse([
+    { firstName: "Harry", lastName: "K.", country: "Brazil", continent: "Americas", age: 19, language: "Python" },
+    { firstName: "Kseniya", lastName: "T.", country: "Belarus", continent: "Europe", age: 29, language: "JavaScript" },
+    { firstName: "Jing", lastName: "X.", country: "China", continent: "Asia", age: 39, language: "Ruby" },
+    { firstName: "Noa", lastName: "A.", country: "Israel", continent: "Asia", age: 40, language: "Ruby" },
+    { firstName: "Andrei", lastName: "E.", country: "Romania", continent: "Europe", age: 59, language: "C" },
+    { firstName: "Maria", lastName: "S.", country: "Peru", continent: "Americas", age: 60, language: "C" },
+    { firstName: "Lukas", lastName: "X.", country: "Croatia", continent: "Europe", age: 75, language: "Python" },
+    { firstName: "Chloe", lastName: "K.", country: "Guernsey", continent: "Europe", age: 88, language: "Ruby" },
+    { firstName: "Viktoria", lastName: "W.", country: "Bulgaria", continent: "Europe", age: 98, language: "PHP" },
+    { firstName: "Piotr", lastName: "B.", country: "Poland", continent: "Europe", age: 128, language: "JavaScript" },
+  ])
+);
